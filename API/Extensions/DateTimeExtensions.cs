63a2f43d3384124dc01abf3423b3dcb30b91d489
vikz95 <viktor.zahariev@protonmail.com>
2020-10-17 11:44:43 +0200

    Extended AppUser Entity. Seeded the DB. Implemented UserRepository. Implemented Mapping from AppUser to MemberDto.

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dayOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dayOfBirth.Year;
+            if (dayOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
