70f5c721e38d2e692728a8a78dbccdc77395e8b4
vikz95 <viktor.zahariev@protonmail.com>
2020-10-17 17:26:00 +0200

    Implemented User lists and detail pages

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
 import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   // ReplaySubject is a buffer, we only want to store the latest value
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
